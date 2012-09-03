5e78245dd4715c31cb1eb030bb13b860d51a86a6
Benjamin P. Jung <headcr4sh@gmail.com>
2012-09-03 16:42:52 +0200

    Add more tests and format code

--- src/test/java/com/cathive/fx/guice/GuiceFXMLLoaderTest.java
+++ src/test/java/com/cathive/fx/guice/GuiceFXMLLoaderTest.java
@@ -31,53 +31,69 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.Module;
 
-@Test
+/**
+ * 
+ * @author Benjamin P. Jung
+ */
+@Test(singleThreaded = true)
 public class GuiceFXMLLoaderTest {
 
-	// Initialized prior to all test methods!
-	private Injector injector = null;
+    // Initialized prior to all test methods!
+    private Injector injector = null;
 
-	private GuiceFXMLLoader fxmlLoader = null;
-	private  ExamplePaneController ctrl;
+    private GuiceFXMLLoader fxmlLoader = null;
+    private ExamplePaneController ctrl;
 
-	@BeforeClass
-	private void initialize() {
-		final Module module = new AbstractModule() {
-			@Override
-			protected void configure() {
-				bind(GuiceFXMLLoader.class);
-			}
-		};
-		this.injector = Guice.createInjector(module); 
-	}
-	
-	@Test(groups = "stage1")
-	public void instantiationTest() throws Exception {
-		// Creates an instance of the GuiceFXMLLoader using a Guice Injector
-		fxmlLoader = injector.getInstance(GuiceFXMLLoader.class);
-		assertNotNull(fxmlLoader);
-	}
+    @BeforeClass
+    private void initialize() {
+        final Module module = new AbstractModule() {
+            @Override
+            protected void configure() {
+                bind(GuiceFXMLLoader.class);
+            }
+        };
+        this.injector = Guice.createInjector(module);
+    }
 
-	@Test(groups = "stage2", dependsOnGroups = "stage1")
-	public void fxmlLoadingTest() throws Exception {
+    @Test(description = "Assert that an instance of the GuiceFXMLLoader without an Injector Instance cannot be created", expectedExceptions = IllegalArgumentException.class)
+    public void instantiationViaConstructorTest() throws Exception {
+        // Constructor call with null argument must fail!
+        new GuiceFXMLLoader(null);
+    }
 
-		// Fetches an instance of the ExamplePaneController and makes sure it is not (yet) initialized.
-		ctrl = injector.getInstance(ExamplePaneController.class);
-		assertNotNull(ctrl);
-		assertTrue(ctrl.isInjected());
-		assertFalse(ctrl.isInitialized());
-		assertNull(ctrl.getRootPane());
-		assertTrue(ctrl.getMethodCalls().contains("postConstruct()"));
-		assertEquals(ctrl.getMethodCalls().size(), 1);
+    @Test(dependsOnMethods = "instantiationViaConstructorTest")
+    public void instantiationViaGuiceTest() throws Exception {
+        // Creates an instance of the GuiceFXMLLoader using a Guice Injector
+        fxmlLoader = injector.getInstance(GuiceFXMLLoader.class);
+        assertNotNull(fxmlLoader);
+    }
 
-		// Load the FXML file and check that afterwards the controller must be initialized.
-		final AnchorPane pane = fxmlLoader.load(getClass().getResource("/ExamplePane.fxml"), ResourceBundle.getBundle("ExamplePane"));
-		assertTrue(ctrl.isInitialized());
-		assertNotNull(ctrl.getRootPane());
-		assertEquals(ctrl.getRootPane(), pane);
-		assertTrue(ctrl.getMethodCalls().contains("initialize(java.net.URL, java.util.ResourceBundle)"));
-		assertEquals(ctrl.getMethodCalls().size(), 2);
+    @Test(dependsOnMethods = "instantiationViaGuiceTest", expectedExceptions = NullPointerException.class)
+    public void fxmlLoadingTestWithNullParams() throws Exception {
+        fxmlLoader.load(null);
+    }
 
-	}
+    @Test(dependsOnMethods = "instantiationViaGuiceTest")
+    public void fxmlLoadingTest() throws Exception {
+
+        // Fetches an instance of the ExamplePaneController and makes sure it is not (yet) initialized.
+        ctrl = injector.getInstance(ExamplePaneController.class);
+        assertNotNull(ctrl);
+        assertTrue(ctrl.isInjected());
+        assertFalse(ctrl.isInitialized());
+        assertNull(ctrl.getRootPane());
+        assertTrue(ctrl.getMethodCalls().contains("postConstruct()"));
+        assertEquals(ctrl.getMethodCalls().size(), 1);
+
+        // Load the FXML file and check that afterwards the controller must be initialized.
+        final AnchorPane pane = fxmlLoader.load(getClass().getResource("/ExamplePane.fxml"),
+                ResourceBundle.getBundle("ExamplePane"));
+        assertTrue(ctrl.isInitialized());
+        assertNotNull(ctrl.getRootPane());
+        assertEquals(ctrl.getRootPane(), pane);
+        assertTrue(ctrl.getMethodCalls().contains("initialize(java.net.URL, java.util.ResourceBundle)"));
+        assertEquals(ctrl.getMethodCalls().size(), 2);
+
+    }
 
 }
