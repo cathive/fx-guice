3378d5e4cb8788436d2c5b23f02e469728857047
Benjamin P. Jung <headcr4sh@gmail.com>
2013-02-15 15:32:40 +0100

    Add Calculator example app

---
+++ examples/calculator-example-app/src/main/java/com/cathive/fx/guice/example/calculator/CalculatorApp.java
@@ -0,0 +1,128 @@
+/*
+ * Copyright (C) 2013 The Cat Hive Developers.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.cathive.fx.guice.example.calculator;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import javafx.application.Application;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.scene.Parent;
+import javafx.scene.SceneBuilder;
+import javafx.stage.Stage;
+import javafx.stage.StageBuilder;
+
+import com.cathive.fx.guice.GuiceApplication;
+import com.cathive.fx.guice.GuiceFXMLLoader;
+import com.google.inject.Inject;
+import com.google.inject.Module;
+import com.google.inject.name.Named;
+
+/**
+ * @author Benjamin P. Jung
+ */
+public final class CalculatorApp extends GuiceApplication {
+
+    @Inject private GuiceFXMLLoader fxmlLoader;
+
+    /** Resource Bundle being used to translate the application */
+    @Inject @Named("resources")
+    private ResourceBundle resources;
+
+    private final ObjectProperty<BigDecimal> xRegister = new SimpleObjectProperty<>(BigDecimal.ZERO);
+    private final ObjectProperty<BigDecimal> yRegister = new SimpleObjectProperty<>(null);
+
+    /**
+     * The function for the calculation is stored here until the calculator needs it.
+     */
+    private final ObjectProperty<Operation> flagRegister = new SimpleObjectProperty<>(null);
+
+    @Override
+    public void init(List<Module> modules) throws Exception {
+        modules.add(new CalculatorAppModule());
+    }
+
+    @Override
+    public void start(final Stage primaryStage) throws Exception {
+
+        final Parent root = fxmlLoader.load(getClass().getResource("CalculatorApp.fxml"), resources).getRoot();
+
+        StageBuilder.create()
+            .title(resources.getString("APP_NAME"))
+            .resizable(false)
+            .scene(SceneBuilder.create()
+                .root(root)
+                .build())
+            .applyTo(primaryStage);
+
+        primaryStage.show();
+
+    }
+
+    public void globalClear() {
+        xRegister.set(BigDecimal.ZERO);
+        yRegister.set(null);
+        flagRegister.set(null);
+    }
+
+    public void setXRegister(final BigDecimal xRegister) {
+        this.xRegister.set(xRegister);
+    }
+
+    public BigDecimal getXRegister() {
+        return this.xRegister.get();
+    }
+
+    public ObjectProperty<BigDecimal> xRegisterProperty() {
+        return this.xRegister;
+    }
+
+    public void setYRegister(BigDecimal yRegister) {
+        this.yRegisterProperty().set(yRegister);
+    }
+
+    public BigDecimal getYRegister() {
+        return this.yRegister.get();
+    }
+
+    public ObjectProperty<BigDecimal> yRegisterProperty() {
+        return this.yRegister;
+    }
+
+    public void setFlagRegister(final Operation operation) {
+        this.flagRegister.set(operation);
+    }
+
+    public Operation getFlagRegister() {
+        return this.flagRegister.get();
+    }
+
+    public ObjectProperty<Operation> flagRegisterProperty() {
+        return this.flagRegister;
+    }
+
+    /**
+     * @param args
+     *        Command line arguments. Currently unused.
+     */
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+}
---
+++ examples/calculator-example-app/src/main/java/com/cathive/fx/guice/example/calculator/CalculatorAppController.java
@@ -0,0 +1,126 @@
+/*
+ * Copyright (C) 2013 The Cat Hive Developers.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.cathive.fx.guice.example.calculator;
+
+import java.math.BigDecimal;
+import java.text.NumberFormat;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+
+import com.cathive.fx.guice.FXMLController;
+import com.google.inject.Inject;
+
+/**
+ * Controller for the calculator app user interface.
+ * @author Benjamin P. Jung
+ */
+@FXMLController
+public final class CalculatorAppController {
+
+    @Inject private CalculatorApp app;
+
+    @FXML private BorderPane rootPane;
+    @FXML private Label output;
+
+    private int decimalPos = 0;
+
+    @FXML
+    public void onDigit(final ActionEvent event) {
+        final Button button = (Button) event.getSource();
+        final Integer digit = (Integer) button.getUserData();
+        final BigDecimal xRegValue = app.getXRegister();
+        if (app.getYRegister() == null && app.getFlagRegister() != null) {
+            app.setYRegister(xRegValue);
+            app.setXRegister(BigDecimal.valueOf(digit));
+            this.decimalPos = 0;
+        } else if (decimalPos > 0) {
+            app.setXRegister(xRegValue.add(BigDecimal.valueOf(digit).divide(BigDecimal.valueOf(decimalPos))));
+            decimalPos *= 10;
+        } else {
+            app.setXRegister(xRegValue.multiply(BigDecimal.valueOf(10)).add(BigDecimal.valueOf(digit)));
+        }
+        updateOutput();
+    }
+
+    @FXML
+    void onDecimalMark(final ActionEvent event) {
+        decimalPos = 10;
+        updateOutput();
+    }
+
+    @FXML
+    void onOperation(final ActionEvent event) {
+        if (app.getYRegister() != null && app.getFlagRegister() != null) {
+            onEnter(event);
+        }
+        final Button button = (Button) event.getSource();
+        final Operation operation = (Operation) button.getUserData();
+        app.setFlagRegister(operation);
+        updateOutput();
+    }
+
+    @FXML
+    void onGlobalClear(final ActionEvent event) {
+        app.globalClear();
+        decimalPos = 0;
+        updateOutput();
+    }
+
+    @FXML
+    void onEnter(final ActionEvent event) {
+
+        if (app.getFlagRegister() == null) {
+            app.setYRegister(null);
+            return;
+        }
+
+        BigDecimal result;
+        switch (app.getFlagRegister()) {
+        case ADD:
+            result = app.getYRegister().add(app.getXRegister());
+            break;
+        case SUBTRACT:
+            result = app.getYRegister().subtract(app.getXRegister()); 
+            break;
+        case MULTIPLY:
+            result = app.getYRegister().multiply(app.getXRegister());
+            break;
+        case DIVIDE:
+            result = app.getYRegister().divide(app.getXRegister());
+            break;
+        default:
+            throw new IllegalStateException();
+        }
+
+        app.setXRegister(result);
+        app.setYRegister(null);
+        app.setFlagRegister(null);
+        decimalPos = 0;
+        updateOutput();
+
+    }
+
+
+    protected void updateOutput() {
+        output.setText(NumberFormat.getInstance().format(app.getXRegister().doubleValue()));
+    }
+
+}
