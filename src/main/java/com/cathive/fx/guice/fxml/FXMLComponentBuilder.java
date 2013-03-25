ca205cfbbd83df79ff37c3254555096771c349d7
Benjamin P. Jung <headcr4sh@gmail.com>
2013-03-25 13:31:01 +0100

    Add more string converter mappings

--- src/main/java/com/cathive/fx/guice/fxml/FXMLComponentBuilder.java
+++ src/main/java/com/cathive/fx/guice/fxml/FXMLComponentBuilder.java
@@ -18,6 +18,8 @@ package com.cathive.fx.guice.fxml;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.AbstractMap;
 import java.util.HashMap;
 import java.util.Map;
@@ -28,12 +30,16 @@ import com.google.inject.Injector;
 
 import javafx.util.Builder;
 import javafx.util.StringConverter;
+import javafx.util.converter.BigDecimalStringConverter;
+import javafx.util.converter.BigIntegerStringConverter;
 import javafx.util.converter.BooleanStringConverter;
+import javafx.util.converter.ByteStringConverter;
 import javafx.util.converter.DefaultStringConverter;
 import javafx.util.converter.DoubleStringConverter;
 import javafx.util.converter.FloatStringConverter;
 import javafx.util.converter.IntegerStringConverter;
 import javafx.util.converter.LongStringConverter;
+import javafx.util.converter.ShortStringConverter;
 
 /**
  * Special builder class for components annotated with {@link FXMLComponent @FXMLComponent}.
@@ -50,18 +56,33 @@ final class FXMLComponentBuilder<T> extends AbstractMap<String, Object> implemen
     
     private static final Map<Class<?>, Class<? extends StringConverter<?>>> STRING_CONVERTERS;
     static {
+
         STRING_CONVERTERS = new HashMap<>();
+
+        // String
         STRING_CONVERTERS.put(String.class, DefaultStringConverter.class);
+
+        // Primitives
         STRING_CONVERTERS.put(boolean.class, BooleanStringConverter.class);
-        STRING_CONVERTERS.put(Boolean.class, BooleanStringConverter.class);
-        STRING_CONVERTERS.put(int.class, IntegerStringConverter.class);
-        STRING_CONVERTERS.put(Integer.class, IntegerStringConverter.class);
-        STRING_CONVERTERS.put(long.class, LongStringConverter.class);
-        STRING_CONVERTERS.put(Long.class, LongStringConverter.class);
-        STRING_CONVERTERS.put(float.class, FloatStringConverter.class);
-        STRING_CONVERTERS.put(Float.class, FloatStringConverter.class);
+        STRING_CONVERTERS.put(byte.class, ByteStringConverter.class);
         STRING_CONVERTERS.put(double.class, DoubleStringConverter.class);
+        STRING_CONVERTERS.put(float.class, FloatStringConverter.class);
+        STRING_CONVERTERS.put(int.class, IntegerStringConverter.class);
+        STRING_CONVERTERS.put(long.class, LongStringConverter.class);
+        STRING_CONVERTERS.put(short.class, ShortStringConverter.class);
+
+        // Primitive wrappers
+        STRING_CONVERTERS.put(Boolean.class, BooleanStringConverter.class);
+        STRING_CONVERTERS.put(Byte.class, ByteStringConverter.class);
         STRING_CONVERTERS.put(Double.class, DoubleStringConverter.class);
+        STRING_CONVERTERS.put(Float.class, FloatStringConverter.class);
+        STRING_CONVERTERS.put(Integer.class, IntegerStringConverter.class);
+        STRING_CONVERTERS.put(Long.class, LongStringConverter.class);
+        STRING_CONVERTERS.put(Short.class, ShortStringConverter.class);
+
+        // Other types
+        STRING_CONVERTERS.put(BigDecimal.class, BigDecimalStringConverter.class);
+        STRING_CONVERTERS.put(BigInteger.class, BigIntegerStringConverter.class);
     }
     
     private Injector injector;
